1c438d3f4808aff488f1f8afa9038b3b24eafcfc
IUUI11 email = 597733372@qq.com <597733372@qq.com>
2020-04-30 21:12:39 +0800

    js

--- src/main/java/xzx/majia/community/community_xzx/controller/IndexController.java
+++ src/main/java/xzx/majia/community/community_xzx/controller/IndexController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
+public class IndexController {
+    @GetMapping("/index")
     public String Hello(@RequestParam(name = "name")String name , Model model){
         model.addAttribute("name",name);
         return "index";
