d5356c4e55afbcd0fedb8a25e398c4bb95e89266
IUUI11 email = 597733372@qq.com <597733372@qq.com>
2020-05-02 23:33:05 +0800

    authorize1

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ## 资料
 [Spring 文档](https://spring.io/guides/)
 [模仿的网址](https://elasticsearch.cn)
+[github outapp文档](ttps://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+[bootstrap文档](ttps://v3.bootcss.com/getting-started/)
 ## 工具
---
+++ src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
@@ -0,0 +1,14 @@
+package xzx.majia.community.community_xzx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        return "index";
+    }
+}
