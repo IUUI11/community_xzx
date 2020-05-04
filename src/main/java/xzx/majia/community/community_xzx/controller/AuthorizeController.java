7f1f84cfe22cabe8b9b11d0c752b9ce7d955d543
IUUI11 email = 597733372@qq.com <597733372@qq.com>
2020-05-04 21:40:58 +0800

    accesstoken

--- src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
+++ src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
@@ -1,14 +1,29 @@
 package xzx.majia.community.community_xzx.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import xzx.majia.community.community_xzx.dto.AccessTokenDto;
+import xzx.majia.community.community_xzx.dto.GithubUser;
+import xzx.majia.community.community_xzx.provider.GithubProvider;
 
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id("267ac43134500d8f60e7");
+        accessTokenDto.setClient_secret("b1f6d6604ecf6fe5049c5e6203da7d42221a950f");
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDto.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
---
+++ src/main/java/xzx/majia/community/community_xzx/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package xzx.majia.community.community_xzx.dto;
+
+public class GithubUser {
+    private long id;
+    private String name;
+    private String bio;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
