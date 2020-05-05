a0b8219087af0de6fdadee4dafa4e8cc6d4f64a0
IUUI11 email = 597733372@qq.com <597733372@qq.com>
2020-05-05 21:18:55 +0800

    extra github config from code to properties

--- src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
+++ src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package xzx.majia.community.community_xzx.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,22 @@ import xzx.majia.community.community_xzx.provider.GithubProvider;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("267ac43134500d8f60e7");
-        accessTokenDto.setClient_secret("b1f6d6604ecf6fe5049c5e6203da7d42221a950f");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
         GithubUser user = githubProvider.getUser(accessToken);
