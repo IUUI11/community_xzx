85355c7525107c573025dd31188df320d56ab938
IUUI11 email = 597733372@qq.com <597733372@qq.com>
2020-05-09 21:58:20 +0800

    mysql 1

--- src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
+++ src/main/java/xzx/majia/community/community_xzx/controller/AuthorizeController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import xzx.majia.community.community_xzx.dto.AccessTokenDto;
 import xzx.majia.community.community_xzx.dto.GithubUser;
+import xzx.majia.community.community_xzx.mapper.UserMapper;
+import xzx.majia.community.community_xzx.model.User;
 import xzx.majia.community.community_xzx.provider.GithubProvider;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -23,6 +26,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -34,10 +40,17 @@ public class AuthorizeController {
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登陆成功 写session和cookie
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         } else {
             //登陆失败 重新登陆
--- src/main/java/xzx/majia/community/community_xzx/mapper/UserMapper.java
+++ src/main/java/xzx/majia/community/community_xzx/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package xzx.majia.community.community_xzx.mapper;
 
-public class UserMapper {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import xzx.majia.community.community_xzx.model.User;
+
+@Mapper
+public interface UserMapper  {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
 }
