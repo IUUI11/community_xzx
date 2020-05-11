b5151af31a6e38114287dd375924a3085e5ef821
IUUI11 email = 597733372@qq.com <597733372@qq.com>
2020-05-11 22:06:21 +0800

    p18

--- src/main/java/xzx/majia/community/community_xzx/controller/IndexController.java
+++ src/main/java/xzx/majia/community/community_xzx/controller/IndexController.java
@@ -1,15 +1,32 @@
 package xzx.majia.community.community_xzx.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import xzx.majia.community.community_xzx.mapper.UserMapper;
+import xzx.majia.community.community_xzx.model.User;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+    @Autowired //request = false
+    private UserMapper userMapper;
     @GetMapping("/index")
-    public String Hello(@RequestParam(name = "name")String name , Model model){
-        model.addAttribute("name",name);
+    public String Hello(HttpServletRequest request){
+        Cookie[] cookies =  request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
--- src/main/java/xzx/majia/community/community_xzx/mapper/UserMapper.java
+++ src/main/java/xzx/majia/community/community_xzx/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package xzx.majia.community.community_xzx.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import xzx.majia.community.community_xzx.model.User;
 
 @Mapper
@@ -9,4 +11,6 @@ public interface UserMapper  {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
